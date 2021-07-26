426aba54accc30c2dc7b90ab0110575153620ca6
Silas Unger <silasburtonunger@gmail.com>
2021-07-25 19:32:22 -0700

    raindrops!

--- projects/shapes.js
+++ projects/shapes.js
@@ -1,25 +1,57 @@
+function RainDrop(in_x, in_y, in_size){
+  this.x = in_x;
+  this.y = in_y;
+  this.size = in_size;
+  this.speed = in_size*13;
+  this.w = random(5) + 10;
+  this.r1 = random(8)-6;
+  this.h_speed = 2*Math.random(in_size) + 1.5;
+  r = random(20) + 20;
+  g = random(30) + 10;
+  b = random(100) + 150;
+  a = random(150) + 100;
+  this.color = color(r, g, b, a);
+
+  this.show = function(){
+    fill(this.color);
+    beginShape();
+    vertex(this.x, this.y);
+    bezierVertex(this.x+10*this.size+this.r1, this.y+32*this.size, this.x+(this.w-1)*this.size, this.y+25*this.size, this.x+this.w*this.size, this.y+40*this.size);
+    bezierVertex(this.x+this.w*this.size, this.y+60*this.size, this.x-this.w*this.size, this.y+60*this.size, this.x-this.w*this.size, this.y+40*this.size);
+    bezierVertex(this.x-(this.w-1)*this.size, this.y+25*this.size, this.x-10*this.size-this.r1, this.y+32*this.size, this.x , this.y );
+    endShape();
+  }
+}
+
+let rain = [];
+let width = screen.width;
+let height = screen.height-170;
+
 function setup() {
-    // Create the canvas
-    createCanvas(720, 400);
-    background(200);
-  
-    // Set colors
-    fill(204, 101, 192, 127);
-    stroke(127, 63, 120);
-  
-    // A rectangle
-    rect(40, 120, 120, 40);
-    // An ellipse
-    ellipse(240, 240, 80, 80);
-    // A triangle
-    triangle(300, 100, 320, 100, 310, 80);
-  
-    // A design for a simple flower
-    translate(580, 200);
-    noStroke();
-    for (let i = 0; i < 10; i ++) {
-      ellipse(0, 30, 20, 80);
-      rotate(PI/5);
+  createCanvas(width, height);
+  frameRate(30);
+  noStroke();
+}
+
+
+function draw() {
+  background(0);
+  for(var i = 0; i < 5; i += 1){
+    if(rain.length < 200){
+      rain.push(new RainDrop(random(width), random(5), random(0.3)+0.4));
     }
   }
-  
+
+  for (let drop of rain) {
+    drop.y += drop.speed;
+    drop.x += drop.h_speed;
+    if(drop.y > height){
+      drop.y -= height;
+    }
+    if(drop.x > width){
+      drop.x -= width;
+    }
+    drop.show();
+  }
+}
+
